52aa2c424003f4a0a2c1c9e303d1ea733c25b9a9
J450N-ong <odeleyejason@gmail.com>
2024-09-29 15:12:11 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,14 @@
+name="John"
+age=1
+if name=="Alex" or name=="John" and age>=5:
+    print("HELLO ",name)
+else:
+    print("GOODBYE ",name)
+ 
+a=1
+b=2
+c=4
+d=8 
+e=a+(c*d*b)
+print("d/b is",d/b)
+print (e)
